35d8d0f79095f206b34a5e90d500e12d767bac60
Steven L. Scott <steve.the.bayesian@gmail.com>
2018-03-21 21:09:49 -0700

    Add C++ support for identifying date ranges where holidays are active.

--- Interfaces/R/bsts/src/bsts_init.cc
+++ Interfaces/R/bsts/src/bsts_init.cc
@@ -23,60 +23,65 @@
 
 extern "C" {
 
-SEXP analysis_common_r_fit_bsts_model_(
-    SEXP r_data_list,
-    SEXP r_state_specification,
-    SEXP r_prior,
-    SEXP r_options,
-    SEXP r_family,
-    SEXP r_save_state_contribution_flag,
-    SEXP r_save_prediction_errors_flag,
-    SEXP r_niter,
-    SEXP r_ping,
-    SEXP r_timeout_in_seconds,
-    SEXP r_seed);
+  SEXP analysis_common_r_fit_bsts_model_(
+      SEXP r_data_list,
+      SEXP r_state_specification,
+      SEXP r_prior,
+      SEXP r_options,
+      SEXP r_family,
+      SEXP r_save_state_contribution_flag,
+      SEXP r_save_prediction_errors_flag,
+      SEXP r_niter,
+      SEXP r_ping,
+      SEXP r_timeout_in_seconds,
+      SEXP r_seed);
 
-SEXP analysis_common_r_predict_bsts_model_(
-    SEXP r_bsts_object,
-    SEXP r_prediction_data,
-    SEXP r_burn,
-    SEXP r_observed_data,
-    SEXP r_seed);
+  SEXP analysis_common_r_predict_bsts_model_(
+      SEXP r_bsts_object,
+      SEXP r_prediction_data,
+      SEXP r_burn,
+      SEXP r_observed_data,
+      SEXP r_seed);
 
-SEXP analysis_common_r_bsts_one_step_prediction_errors_(
-    SEXP r_bsts_object,
-    SEXP r_cutpoints);
+  SEXP analysis_common_r_bsts_one_step_prediction_errors_(
+      SEXP r_bsts_object,
+      SEXP r_cutpoints);
 
-SEXP analysis_common_r_bsts_aggregate_time_series_(
-    SEXP r_fine_series,
-    SEXP r_contains_end,
-    SEXP r_membership_fraction);
+  SEXP analysis_common_r_bsts_aggregate_time_series_(
+      SEXP r_fine_series,
+      SEXP r_contains_end,
+      SEXP r_membership_fraction);
 
-SEXP analysis_common_r_bsts_fit_mixed_frequency_model_(
-    SEXP r_target_series,
-    SEXP r_predictors,
-    SEXP r_which_coarse_interval,
-    SEXP r_membership_fraction,
-    SEXP r_contains_end,
-    SEXP r_state_specification,
-    SEXP r_regression_prior,
-    SEXP r_niter,
-    SEXP r_ping,
-    SEXP r_seed,
-    SEXP r_truth);
+  SEXP analysis_common_r_bsts_fit_mixed_frequency_model_(
+      SEXP r_target_series,
+      SEXP r_predictors,
+      SEXP r_which_coarse_interval,
+      SEXP r_membership_fraction,
+      SEXP r_contains_end,
+      SEXP r_state_specification,
+      SEXP r_regression_prior,
+      SEXP r_niter,
+      SEXP r_ping,
+      SEXP r_seed,
+      SEXP r_truth);
 
-static R_CallMethodDef bsts_arg_description[] = {
-  CALLDEF(analysis_common_r_fit_bsts_model_, 11),
-  CALLDEF(analysis_common_r_predict_bsts_model_, 5),
-  CALLDEF(analysis_common_r_bsts_one_step_prediction_errors_, 2),
-  CALLDEF(analysis_common_r_bsts_aggregate_time_series_, 3),
-  CALLDEF(analysis_common_r_bsts_fit_mixed_frequency_model_, 11),
-  {NULL, NULL, 0}  // NOLINT
-};
+  SEXP analysis_common_r_get_date_ranges_(
+      SEXP r_holiday,
+      SEXP r_timestamps);
+  
+  static R_CallMethodDef bsts_arg_description[] = {
+    CALLDEF(analysis_common_r_fit_bsts_model_, 11),
+    CALLDEF(analysis_common_r_predict_bsts_model_, 5),
+    CALLDEF(analysis_common_r_bsts_one_step_prediction_errors_, 2),
+    CALLDEF(analysis_common_r_bsts_aggregate_time_series_, 3),
+    CALLDEF(analysis_common_r_bsts_fit_mixed_frequency_model_, 11),
+    CALLDEF(analysis_common_r_get_date_ranges_, 2),
+    {NULL, NULL, 0}  // NOLINT
+  };
 
-void R_init_bsts(DllInfo *info) {
-  R_registerRoutines(info, NULL, bsts_arg_description, NULL, NULL);  // NOLINT
-  R_useDynamicSymbols(info, FALSE);
-}
+  void R_init_bsts(DllInfo *info) {
+    R_registerRoutines(info, NULL, bsts_arg_description, NULL, NULL);  // NOLINT
+    R_useDynamicSymbols(info, FALSE);
+  }
 
 }  // extern "C"
