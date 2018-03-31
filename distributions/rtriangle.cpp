78c950b62c07b666c5991bce2e31229a8e46acd4
Steven L. Scott <steve.the.bayesian@gmail.com>
2018-03-30 22:26:57 -0700

    Should be the final codacy code quality issue.

--- distributions/rtriangle.cpp
+++ distributions/rtriangle.cpp
@@ -120,18 +120,18 @@ namespace BOOM {
     y = 2.0 / (x1 - x0);
 
     double ans = 0;
-    if (x <= xm) {
+    if (!std::isfinite(x) || !std::isfinite(xm)) {
+      report_error("Non-finite inputs to ptriangle.");
+    } else if (x <= xm) {
       x -= x0;
       double m0 = y / (xm - x0);
       double a0 = 0.5 * m0 * x * x;
       ans = lower_tail ? a0 : 1 - a0;
-    } else if (x > xm) {
+    } else {
       double m1 = y / (xm - x1);
       double b = 0.5 * m1 * (x - x1) * (x1 - x);
       ans = lower_tail ? b : 1 - b;
-    } else
-      report_error("an unknown error occurred in ptrigangle");
-
+    }
     return ans;
   }
   /*======================================================================*/
