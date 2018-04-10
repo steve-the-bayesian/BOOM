97373b6ccafaa616c686c81338cb01301f12d372
Steven L. Scott <steve.the.bayesian@gmail.com>
2018-04-10 00:07:15 -0700

    A more robust implementation of PoissonDataImputer when log lambda is very large in either direction.

--- Models/Glm/PosteriorSamplers/PoissonDataImputer.cpp
+++ Models/Glm/PosteriorSamplers/PoissonDataImputer.cpp
@@ -21,24 +21,63 @@
 #include <cmath>
 #include "Models/Glm/PosteriorSamplers/poisson_mixture_approximation_table.hpp"
 #include "distributions.hpp"
+#include "cpputil/lse.hpp"
 
 namespace BOOM {
 
+  // Returns true if x can be exponentiated without numerical problems.
+  inline bool safe_to_exp(double x) {
+    return fabs(x) < 600;
+  }
+  
   NormalMixtureApproximationTable PoissonDataImputer::mixture_table_(
       create_poisson_mixture_approximation_table());
 
-  void PoissonDataImputer::impute(RNG &rng, int response, double exposure,
+  void PoissonDataImputer::impute(RNG &rng,
+                                  int response,
+                                  double exposure,
                                   double log_lambda,
                                   double *internal_neglog_final_event_time,
-                                  double *internal_mu, double *internal_weight,
+                                  double *internal_mu,
+                                  double *internal_weight,
                                   double *neglog_final_interarrival_time,
                                   double *external_mu,
                                   double *external_weight) {
     double time_of_final_internal_event =
-        response > 0 ? exposure * (rbeta_mt(rng, response, 1)) : 0;
-    double final_interarrival_time =
-        exposure - time_of_final_internal_event + rexp_mt(rng, exp(log_lambda));
-    double z_external = -log(final_interarrival_time);
+          response > 0 ? exposure * (rbeta_mt(rng, response, 1)) : 0;
+
+    // Delta is the time between the final interior event time and the end of
+    // the interval.
+    double delta = exposure - time_of_final_internal_event;
+    double final_interarrival_time, z_external;
+    if (safe_to_exp(log_lambda)) {
+      final_interarrival_time = delta + rexp_mt(rng, exp(log_lambda));
+      z_external = -log(final_interarrival_time);
+    } else {
+      if (delta > 0) {
+        // Handle cases where log lambda is really big or small.  Really big
+        // cases are fine.  We just need to keep the algorithm from crashing.
+        // Fast mixing will take you back to mortal sized values soon.  When
+        // log-lambda is small the algorithm can be drawn in to a slow-mixing
+        // cycle because very small log-lambda values lead to very large latent
+        // data.
+        //
+        // Here's the logic: Let D be the distance between the final event time
+        // in the interval, and the end of the interval.  You're going to add an
+        // E(lambda) = E(1) / lambda to D to get to the final event, then take
+        // the negative log of D + E/lambda to get z.  The negative log of an
+        // E(1) is standard extreme value, so the negative of a standard extreme
+        // value is the log of an E(1).
+        //
+        // z = -log(D + E/lambda)
+        //   = -lse2( log(D), log(E) - log_lambda)
+        //   = -lse2( log(D),  -StandardExv - log(lambda))
+        double error = rexv_mt(rng, 0, 1);
+        z_external = -lse2(log(delta), -error - log_lambda);
+      } else {
+        z_external = log_lambda + rexv_mt(rng, 0, 1);
+      }
+    }
     double mu = 0;
     double sigsq = 1;
     unmix_poisson_augmented_data(rng, z_external - log_lambda, 1, &mu, &sigsq,
