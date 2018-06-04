847c82e4bb075bdae5bb29b8b50af90af147f83d
Steven L. Scott <stb@daedalean.ai>
2018-06-04 15:36:43 -0700

    Reformat a bunch of comments.  No actual code changes.

--- Models/Glm/PosteriorSamplers/BregVsSampler.hpp
+++ Models/Glm/PosteriorSamplers/BregVsSampler.hpp
@@ -55,12 +55,11 @@ namespace BOOM {
   // slopes to zero.
   class BregVsSampler : public PosteriorSampler {
    public:
-    // Omega inverse is 'prior_nobs' * XTX/n. The intercept term in
-    // 'b' is ybar (sample mean of the responses).  The slope terms in
-    // b are all zero.  The prior for 1/sigsq is
-    // Gamma(prior_nobs/2, prior_ss/2), with
-    // prior_ss = prior_nobs*sigma_guess^2, and
-    // sigma_guess = sample_variance*(1-expected_rsq)
+    // Omega inverse is 'prior_nobs' * XTX/n. The intercept term in 'b' is ybar
+    // (sample mean of the responses).  The slope terms in b are all zero.  The
+    // prior for 1/sigsq is Gamma(prior_nobs/2, prior_ss/2), with prior_ss =
+    // prior_nobs*sigma_guess^2, and sigma_guess =
+    // sample_variance*(1-expected_rsq)
     BregVsSampler(RegressionModel *model,
                   double prior_nobs,           // Omega is prior_nobs * XTX/n
                   double expected_rsq,         // sigsq_guess = sample var*this
@@ -68,35 +67,34 @@ namespace BOOM {
                   bool first_term_is_intercept = true,
                   RNG &seeding_rng = GlobalRng::rng);
 
-    // Omega inverse is kappa*[(1-alpha)*XTX/n + alpha*(XTX/n)].
-    // kappa is 'prior_beta_nobs', and alpha is 'diagonal_shrinkage'.
-    // The prior on 1/sigsq is Gamma(prior_sigma_nobs/2, priors_ss/2)
-    // with prior_ss = prior_sigma_guess^2 * prior_sigma_nobs.
-    // b = [ybar, 0, 0, ...]
+    // Omega inverse is kappa*[(1-alpha)*XTX/n + alpha*(XTX/n)].  kappa is
+    // 'prior_beta_nobs', and alpha is 'diagonal_shrinkage'.  The prior on
+    // 1/sigsq is Gamma(prior_sigma_nobs/2, priors_ss/2) with prior_ss =
+    // prior_sigma_guess^2 * prior_sigma_nobs.  b = [ybar, 0, 0, ...]
     BregVsSampler(RegressionModel *model, double prior_sigma_nobs,
                   double prior_sigma_guess, double prior_beta_nobs,
                   double diagonal_shrinkage, double prior_inclusion_probability,
                   bool force_intercept = true,
                   RNG &seeding_rng = GlobalRng::rng);
 
-    // Use this constructor if you want full control over the
-    // parameters of the prior distribution, but you don't want to
-    // supply actual model objects.  You won't be able to modify the
-    // values of the prior parameters afterwards.
+    // Use this constructor if you want full control over the parameters of the
+    // prior distribution, but you don't want to supply actual model objects.
+    // You won't be able to modify the values of the prior parameters
+    // afterwards.
     BregVsSampler(RegressionModel *model, const Vector &prior_mean,
                   const SpdMatrix &unscaled_prior_precision, double sigma_guess,
                   double df, const Vector &prior_inclusion_probs,
                   RNG &seeding_rng = GlobalRng::rng);
 
-    // Equivalent to the preceding constructor, but the prior
-    // parameters are specified in a struct.
+    // Equivalent to the preceding constructor, but the prior parameters are
+    // specified in a struct.
     BregVsSampler(RegressionModel *model, const ZellnerPriorParameters &prior,
                   RNG &seeding_rng = GlobalRng::rng);
 
-    // This constructor offers full control.  If external copies of
-    // the pointers supplied to the constructor are kept then the
-    // values of the prior parameters can be modified.  This would be
-    // useful in a hierarchical model, for example.
+    // This constructor offers full control.  If external copies of the pointers
+    // supplied to the constructor are kept then the values of the prior
+    // parameters can be modified.  This would be useful in a hierarchical
+    // model, for example.
     BregVsSampler(RegressionModel *model,
                   const Ptr<MvnGivenScalarSigmaBase> &slab,
                   const Ptr<GammaModelBase> &residual_precision_prior,
@@ -126,21 +124,20 @@ namespace BOOM {
     double logpri() const override;
     double log_model_prob(const Selector &inclusion_indicators) const;
 
-    // Model selection can be turned on and off altogether, or if very
-    // large sets of predictors are being considered then the number
-    // of exploration steps can be limited to a specified number.
+    // Model selection can be turned on and off altogether, or if very large
+    // sets of predictors are being considered then the number of exploration
+    // steps can be limited to a specified number.
     void suppress_model_selection();
 
-    // allow_model_selection takes an argument so its signature will
-    // match other similar spike and slab sampler classes.
+    // allow_model_selection takes an argument so its signature will match other
+    // similar spike and slab sampler classes.
     void allow_model_selection(bool allow = true);
 
-    // Restrict model selection to be no more than 'nflips' locations
-    // per sweep.
+    // Restrict model selection to be no more than 'nflips' locations per sweep.
     void limit_model_selection(uint nflips);
 
-    // For testing purposes, the draw of beta and/or sigma can be
-    // suppressed.  This is also useful in cases where sigma is known.
+    // For testing purposes, the draw of beta and/or sigma can be suppressed.
+    // This is also useful in cases where sigma is known.
     void suppress_beta_draw();
     void suppress_sigma_draw();
     void allow_sigma_draw();
@@ -153,11 +150,10 @@ namespace BOOM {
 
     void set_sigma_upper_limit(double sigma_upper_limit);
 
-    // Sets the model parameters to their posterior mode, conditional
-    // on the current include / exclude status of the regression
-    // coefficients.  Any coefficient that is included will be
-    // optimized.  Any coefficient that is excluded will continue to
-    // be set to zero.
+    // Sets the model parameters to their posterior mode, conditional on the
+    // current include / exclude status of the regression coefficients.  Any
+    // coefficient that is included will be optimized.  Any coefficient that is
+    // excluded will continue to be set to zero.
     void find_posterior_mode(double epsilon = 1e-5) override;
 
     bool can_find_posterior_mode() const override { return true; }
@@ -169,21 +165,21 @@ namespace BOOM {
       return sigsq_sampler_.draw(rng(), df, ss);
     }
 
-    // Does one MCMC draw on a specific element of a vector of
-    // inclusion indicators, given all others.
+    // Does one MCMC draw on a specific element of a vector of inclusion
+    // indicators, given all others.
+
     // Args:
-    //   inclusion_indicators: The vector of inclusion indicators to
-    //     be sampled.
-    //   which_var: The position (element) in inclusion_indicators
-    //     that might be changed.
+
+    //   inclusion_indicators: The vector of inclusion indicators to be sampled.
+    //   which_var: The position (element) in inclusion_indicators that might be
+    //     changed.
     //   current_logp: The current log posterior evaluated at
     //     inclusion_indicators.
     //
     // Returns:
     //   inclusion_indicators[which_var] will be sampled from its full
-    //   conditional distribution.  The return value is the
-    //   (unnormalized) log posterior of the current
-    //   inclusion_indicators at function exit.
+    //   conditional distribution.  The return value is the (unnormalized) log
+    //   posterior of the current inclusion_indicators at function exit.
     double mcmc_one_flip(Selector &inclusion_indicators, uint which_var,
                          double current_logp);
 
@@ -191,15 +187,15 @@ namespace BOOM {
     // The model whose paramaters are to be drawn.
     RegressionModel *model_;
 
-    // A conditionally (given sigma) Gaussian prior distribution for
-    // the coefficients of the full model (with all variables included).
+    // A conditionally (given sigma) Gaussian prior distribution for the
+    // coefficients of the full model (with all variables included).
     Ptr<MvnGivenScalarSigmaBase> slab_;
 
     // A marginal prior distribution for 1/sigma^2.
     Ptr<GammaModelBase> residual_precision_prior_;
 
-    // A marginal prior for the set of 0's and 1's indicating which
-    // variables are in/out of the model.
+    // A marginal prior for the set of 0's and 1's indicating which variables
+    // are in/out of the model.
     Ptr<VariableSelectionPrior> spike_;
 
     std::vector<uint> indx;
@@ -239,5 +235,7 @@ namespace BOOM {
     const Ptr<VariableSelectionPrior> &check_spike_dimension(
         const Ptr<VariableSelectionPrior> &spike);
   };
+  
 }  // namespace BOOM
+
 #endif  // BOOM_BREG_VS_SAMPLER_HPP
