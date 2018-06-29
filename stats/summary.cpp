3cda9f4733f537e2383548e2c18f8f68e3e46467
Steven L. Scott <steve.the.bayesian@gmail.com>
2018-06-29 13:13:03 -0700

    A class that summarizes numerical data.  Useful for logging and testing.

---
+++ stats/summary.cpp
@@ -0,0 +1,56 @@
+/*
+  Copyright (C) 2005-2018 Steven L. Scott
+
+  This library is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation; either version 2.1 of the License, or (at your option)
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with this library; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#include "stats/summary.hpp"
+#include "stats/ECDF.hpp"
+#include "stats/moments.hpp"
+
+#include <iomanip>
+
+namespace BOOM {
+
+  NumericSummary::NumericSummary(const ConstVectorView &data) {
+    ECDF ecdf(data);
+    min_ = ecdf.sorted_data()[0];
+    lower_quartile_ = ecdf.quantile(.25);
+    median_ = ecdf.quantile(.5);
+    mean_ = mean(data);
+    upper_quartile_ = ecdf.quantile(.75);
+    max_ = ecdf.sorted_data().back();
+  }
+
+  std::string NumericSummary::to_string() const {
+    std::ostringstream out;
+    print(out);
+    return out.str();
+  }
+  
+  std::ostream &NumericSummary::print(std::ostream &out) const {
+    using std::endl;
+    auto precision = out.precision();
+    out <<  "min:            " << std::setprecision(4) << min_ << endl
+        <<  "lower quartile: " << lower_quartile_ << endl
+        <<  "median:         " << median_ << endl
+        <<  "mean:           " << mean_ << endl
+        <<  "upper quartile: " << upper_quartile_ << endl
+        <<  "max:            " << max_ << endl;
+    out << std::setprecision(precision);
+    return out;
+  }
+  
+} // namespace BOOM
---
+++ stats/summary.hpp
@@ -0,0 +1,50 @@
+#ifndef BOOM_STATS_SUMMARY_HPP_
+#define BOOM_STATS_SUMMARY_HPP_
+
+/*
+  Copyright (C) 2005-2018 Steven L. Scott
+
+  This library is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation; either version 2.1 of the License, or (at your option)
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with this library; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#include "LinAlg/VectorView.hpp"
+#include <string>
+
+namespace BOOM {
+
+  // A collection of quantiles, plus the mean, for a numeric vector of data.
+  class NumericSummary {
+   public:
+    NumericSummary(const ConstVectorView &data);
+    std::string to_string() const;
+    std::ostream &print(std::ostream &out) const;
+
+   private:
+    double min_;
+    double lower_quartile_;
+    double median_;
+    double mean_;
+    double upper_quartile_;
+    double max_;
+  };
+
+  std::ostream &operator<<(std::ostream &out, const NumericSummary &summary) {
+    return summary.print(out);
+  }
+  
+} // namespace BOOM
+
+#endif //  BOOM_STATS_SUMMARY_HPP_
+
