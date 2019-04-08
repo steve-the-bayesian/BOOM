8aa02ea722b30c3d4730cc0dd308c72d8d653d77
Steven L. Scott <steve.the.bayesian@gmail.com>
2019-04-08 16:20:16 -0700

    Add the ability to reset the X and Y smart pointers in GlmData.

--- Models/Glm/Glm.hpp
+++ Models/Glm/Glm.hpp
@@ -54,6 +54,12 @@ namespace BOOM {
     Ptr<VectorData> Xptr() { return x_; }
     const Ptr<VectorData> Xptr() const { return x_; }
 
+    // Replace the representation of x_ by a new pointer.  This is an unusual
+    // operation.
+    void set_Xptr(const Ptr<VectorData> &x) {
+      x_ = x;
+    }
+    
    private:
     Ptr<VectorData> x_;
   };
@@ -87,6 +93,12 @@ namespace BOOM {
 
     Ptr<DAT> Yptr() { return y_; }
 
+    // Replace the representation of the response with a new object.  This is an
+    // unusual operation.
+    void set_Yptr(const Ptr<DAT> &y) {
+      y_ = y;
+    }
+    
    private:
     // If an intercept is desired, it must be explicitly included.
     Ptr<DAT> y_;
