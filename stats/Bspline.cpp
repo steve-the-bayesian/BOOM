51190e4bbb31445773d5f4923b7541699ef9d7c9
Steve Scott <stevescott@steves-mbp.lan>
2019-07-15 11:16:11 -0700

    Update comments for Bspline to use 80 columns.

--- stats/Bspline.cpp
+++ stats/Bspline.cpp
@@ -31,14 +31,14 @@ namespace BOOM {
       report_error("Spline degree must be non-negative.");
     }
     if (sorted_knots.size() <= 1) {
-      // If the knot vector contains 0 or 1 knots then the "spline
-      // basis expansion" of x is just the empty vector.
+      // If the knot vector contains 0 or 1 knots then the "spline basis
+      // expansion" of x is just the empty vector.
       basis_dimension_ = 0;
     } else {
-      // There are number_of_knots() - 1 knot spans, each with its own
-      // basis function.  Each degree of the spline adds one more
-      // basis element, from the basis functions corresponding to the
-      // additional fake knots at knots[0].
+      // There are number_of_knots() - 1 knot spans, each with its own basis
+      // function.  Each degree of the spline adds one more basis element, from
+      // the basis functions corresponding to the additional fake knots at
+      // knots[0].
       basis_dimension_ = sorted_knots.size() - 1 + degree;
     }
     for (int i = 1; i < sorted_knots.size(); ++i) {
@@ -64,28 +64,26 @@ namespace BOOM {
     }
 
     const Vector &sorted_knots(SplineBase::knots());
-    // To find the knot in the left endpoint of the knot span, we
-    // first find the first knot larger than x, then back up one spot.
+    // To find the knot in the left endpoint of the knot span, we first find the
+    // first knot larger than x, then back up one spot.
     Vector::const_iterator terminal_knot_position =
         std::upper_bound(sorted_knots.begin(), sorted_knots.end(), x);
     int terminal_knot = terminal_knot_position - sorted_knots.begin();
     int knot_span_for_x = terminal_knot - 1;
 
-    // Each row of the basis_function_table corresponds to one knot
-    // span.  Row zero corresponds to [sorted_knots[0],
-    // sorted_knots[1]), row 1 to [sorted_knots[1], sorted_knots[2]),
-    // etc.  The columns correspond to the degree of the spline.  We
-    // first compute the zero-degree bases.  We can use the
-    // zero-degree bases to get the first degree bases, etc.
+    // Each row of the basis_function_table corresponds to one knot span.  Row
+    // zero corresponds to [sorted_knots[0], sorted_knots[1]), row 1 to
+    // [sorted_knots[1], sorted_knots[2]), etc.  The columns correspond to the
+    // degree of the spline.  We first compute the zero-degree bases.  We can
+    // use the zero-degree bases to get the first degree bases, etc.
     //
-    // The zero-degree basis is 1 in exactly one position (the knot
-    // span containing x).  The linear basis is nonzero in two
-    // positions: knot span containing x and the the one prior to
-    // that.  Each additional degree increases the number of nonzero
-    // knot-spans by 1.
+    // The zero-degree basis is 1 in exactly one position (the knot span
+    // containing x).  The linear basis is nonzero in two positions: knot span
+    // containing x and the the one prior to that.  Each additional degree
+    // increases the number of nonzero knot-spans by 1.
     //
-    // Given the initial (zero-degree) basis, we can compute each
-    // higher degree basis using the recurrence relation:
+    // Given the initial (zero-degree) basis, we can compute each higher degree
+    // basis using the recurrence relation:
     //   basis[knot_span, degree](x) =
     //       L(x, knot, degree) * basis(knot, degree - 1)
     //      +R(x, knot, degree) * basis(knot + 1, degree - 1)
@@ -95,29 +93,28 @@ namespace BOOM {
     // L(x, knot, degree) =   ---------------------
     //                  knots[knot + degree] - knots[knot]
     //
-    // if the denominator is zero (because of knot multiplicity) then
-    // the value of L is arbitrary, so we set it to zero.
+    // if the denominator is zero (because of knot multiplicity) then the value
+    // of L is arbitrary, so we set it to zero.
     //
     // and the right coefficient R is
     //     R(x, knot, degree) = 1 - L(x, knot + 1, degree).
     //
-    // All this is explained in deBoor ("A Practical Guide to
-    // Splines") chapter IX, and online at
+    // All this is explained in deBoor ("A Practical Guide to Splines") chapter
+    // IX, and online at
     // www.cs.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/B-spline/bspline-basis.html
 
     int number_of_knot_spans = number_of_knots() - 1;
 
-    // The entries of basis_function_table, column d, are the spline
-    // basis functions of degree d.
+    // The entries of basis_function_table, column d, are the spline basis
+    // functions of degree d.
     ArbitraryOffsetMatrix basis_function_table(
         -degree(), number_of_knot_spans + order_, 0, order_, 0.0);
 
     basis_function_table(knot_span_for_x, 0) = 1.0;
     for (int d = 1; d <= degree(); ++d) {
-      // For a given knot_span containing x, each spline basis of
-      // degree d is nonzero over its knot span, and the next d spans.
-      // Thus to find the set of nonzero bases containing x, we must
-      // look _back_ d spaces.
+      // For a given knot_span containing x, each spline basis of degree d is
+      // nonzero over its knot span, and the next d spans.  Thus to find the set
+      // of nonzero bases containing x, we must look _back_ d spaces.
       for (int lag = 0; lag <= d; ++lag) {
         int span = knot_span_for_x - lag;
         double left_coefficient = compute_coefficient(x, span, d);
@@ -144,8 +141,8 @@ namespace BOOM {
       double dknot = knot(knot_span + degree) - knot(knot_span);
       return (x - knot(knot_span)) / dknot;
     } else {
-      // In this case the result is arbitrary, because the coefficient
-      // will only multiply basis functions with the value zero.
+      // In this case the result is arbitrary, because the coefficient will only
+      // multiply basis functions with the value zero.
       // http://math.stackexchange.com/questions/52157/can-cox-de-boor-recursion-formula-apply-to-b-splines-with-multiple-knots
       return 0;
     }
--- stats/Bspline.hpp
+++ stats/Bspline.hpp
@@ -25,47 +25,41 @@
 
 namespace BOOM {
 
-  // Compute a Bspline basis expansion of a scalar value x.  A Bspline
-  // is a spline formed by a set of local basis functions (the
-  // b-spline basis) determined by a set of knots.  The knots
-  // partition an interval [lo, hi] over which the spline function
-  // nonzero.  The spline basis is zero over (-infinity, lo) and (hi,
-  // infinity).
+  // Compute a Bspline basis expansion of a scalar value x.  A Bspline is a
+  // spline formed by a set of local basis functions (the b-spline basis)
+  // determined by a set of knots.  The knots partition an interval [lo, hi]
+  // over which the spline function nonzero.  The spline basis is zero over
+  // (-infinity, lo) and (hi, infinity).
   //
-  // To make the B-spline theory work, the notional set of knots is
-  // supposed to be infinite, but in practice a B-spline basis
-  // function of degree d is nonzero over at most d+1 knot spans.
-  // This means we can add d+1 fake knots at the beginning and the end
-  // of the knot sequence, and the knots can be in any arbitrary
-  // positions.  This class follows an established convention of
-  // adding the fake knots at the first and last elements of the knot
-  // vector.
+  // To make the B-spline theory work, the notional set of knots is supposed to
+  // be infinite, but in practice a B-spline basis function of degree d is
+  // nonzero over at most d+1 knot spans.  This means we can add d+1 fake knots
+  // at the beginning and the end of the knot sequence, and the knots can be in
+  // any arbitrary positions.  This class follows an established convention of
+  // adding the fake knots at the first and last elements of the knot vector.
   class Bspline : public SplineBase {
    public:
     // Args:
-    //   knots: The set of knots for the spline.  In between pairs of
-    //     knots, the spline is a piecewise polynomial whose degree is
-    //     given by the second argument.  The first and last knots
-    //     define the interval over which the spline is defined.
-    //     These knots are implicitly replicated an infinite number of
-    //     times.
-    //   degree: The degree of the piecewise polynomial in between
-    //     pairs of interior knots.
+    //   knots: The set of knots for the spline.  In between pairs of knots, the
+    //     spline is a piecewise polynomial whose degree is given by the second
+    //     argument.  The first and last knots define the interval over which
+    //     the spline is defined.  These knots are implicitly replicated an
+    //     infinite number of times.
+    //   degree: The degree of the piecewise polynomial in between pairs of
+    //     interior knots.
     explicit Bspline(const Vector &knots, int degree = 3);
 
-    // The Bspline basis expansion at the value x.  If x lies outside
-    // the range [knots.begin(), knots.end()] then all basis elements
-    // are zero.
+    // The Bspline basis expansion at the value x.  If x lies outside the range
+    // [knots.begin(), knots.end()] then all basis elements are zero.
     //
     // If there are fewer than 2 knots then the return value is empty.
     Vector basis(double x) const override;
 
-    // The dimension of the spline basis, which is one for every
-    // distinct interval covered by knots(), plus one for every degree
-    // of the piecewise polynomial.  Normally this is number_of_knots
-    // - 1 + degree, though it can be less if knots() contains
-    // duplicate elements.  If knots().size <= 1 then the
-    // basis_dimension is 0.
+    // The dimension of the spline basis, which is one for every distinct
+    // interval covered by knots(), plus one for every degree of the piecewise
+    // polynomial.  Normally this is number_of_knots - 1 + degree, though it can
+    // be less if knots() contains duplicate elements.  If knots().size <= 1
+    // then the basis_dimension is 0.
     int basis_dimension() const override { return basis_dimension_; }
 
     // The order of the piecewise polynomial connecting the knots.
@@ -83,8 +77,7 @@ namespace BOOM {
     double compute_coefficient(double x, int knot_span, int degree) const;
 
    private:
-    // The order (1 + degree) of the piecewise polynomial connecting
-    // the knots.
+    // The order (1 + degree) of the piecewise polynomial connecting the knots.
     int order_;
 
     // The dimension of the spline basis expansion.
