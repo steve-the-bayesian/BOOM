805482163367af373f8e39d01e1f666a79d104b2
Steven L. Scott <steve.the.bayesian@gmail.com>
2020-04-04 22:22:25 -0700

    Add a static marginal log likelihood function for regression models.

--- Models/Glm/RegressionModel.cpp
+++ Models/Glm/RegressionModel.cpp
@@ -116,6 +116,8 @@ namespace BOOM {
   }
 
   namespace {
+    const double log2pi = 1.83787706640935;
+
     Vector ColSums(const Matrix &m) {
       Vector one(nrow(m), 1.0);
       return one * m;
@@ -618,7 +620,6 @@ namespace BOOM {
     b.pop_back();
     if (b.empty()) return empty_loglike(g, h, nd);
     double n = suf()->n();
-    const double log2pi = 1.83787706640935;
     double SSE = yty() - 2 * b.dot(xty()) + xtx().Mdist(b);
     // It is tempting to compute ans using log_likelihood(), but some
     // of the pieces are also needed for derivatives, and we don't
@@ -653,6 +654,43 @@ namespace BOOM {
     const double log2pi = 1.83787706640935;
     return -.5 * (n * log2pi + n * log(sigsq) + SSE / sigsq);
   }
+
+  double RM::marginal_log_likelihood(
+      double sigsq,
+      const SpdMatrix &xtx,
+      const Vector &xty,
+      double yty,
+      double n,
+      const Vector &prior_mean,
+      const Matrix &unscaled_prior_variance_lower_cholesky,
+      const Vector &posterior_mean,
+      const Matrix &unscaled_posterior_precision_cholesky) {
+
+    // SSE is the sum of square errors when evaluated at the
+    // posterior mean of beta.
+    double SSE = xtx.Mdist(posterior_mean) - 2 * posterior_mean.dot(xty) + yty;
+
+    // SSP is the Mahalanobis distance from the prior to the posterior mean,
+    // relative to the unscaled prior precision.
+    //
+    // If omega = LL' then ominv = L'^-1 L^-1
+    // Then b' ominv b = B'B where B = L^-1 b.
+    Vector prior_posterior_distance = Lsolve(
+        unscaled_prior_variance_lower_cholesky,
+        (prior_mean - posterior_mean));
+    double SSP = prior_posterior_distance.dot(prior_posterior_distance);
+
+    double ans = -.5 * n * (log2pi + log(sigsq));
+    // The log determinant of ominv is -1 * the log determinant of Omega, so the
+    // two determinants have the same sign below.
+    //
+    // Also the log determinant of ominv is twice the log of the sum of the
+    ans -= sum(log(abs(unscaled_prior_variance_lower_cholesky.diag())));
+    ans -= sum(log(abs(unscaled_posterior_precision_cholesky.diag())));
+    ans -= 0.5 * (SSE + SSP) / sigsq;
+    return ans;
+  }
+
 
   // Log likelihood when beta is empty, so that xbeta = 0.  In this
   // case the only parameter is sigma^2
