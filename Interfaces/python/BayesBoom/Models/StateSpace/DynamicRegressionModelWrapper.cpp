b336e70820c6d74d247b12a6c33c1182780c7077
Steven L. Scott <steve.the.bayesian@gmail.com>
2020-04-15 14:20:10 -0700

    Python smoke test passes.

--- Interfaces/python/BayesBoom/Models/StateSpace/DynamicRegressionModelWrapper.cpp
+++ Interfaces/python/BayesBoom/Models/StateSpace/DynamicRegressionModelWrapper.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 
 #include "Models/StateSpace/DynamicRegression.hpp"
+#include "Models/StateSpace/PosteriorSamplers/DynamicRegressionDirectGibbs.hpp"
 
 #include "cpputil/Ptr.hpp"
 
@@ -10,6 +11,10 @@ PYBIND11_DECLARE_HOLDER_TYPE(T, BOOM::Ptr<T>, true);
 namespace BayesBoom {
   using namespace BOOM;
   using namespace BOOM::StateSpace;
+
+  namespace {
+    using DRDGS = DynamicRegressionDirectGibbsSampler;
+  }
 
   void DynamicRegressionModel_def(py::module &boom) {
 
@@ -38,7 +43,8 @@ namespace BayesBoom {
     py::class_<DynamicRegressionModel,
                PriorPolicy,
                BOOM::Ptr<DynamicRegressionModel>>(boom, "DynamicRegressionModel")
-        .def(py::init<int>(), py::arg("xdim"),
+        .def(py::init<int>(),
+             py::arg("xdim"),
              "Args:\n\n"
              "  xdim: Number of potential predictor variables.")
         .def_property_readonly(
@@ -51,6 +57,13 @@ namespace BayesBoom {
               return ans;},
             "Matrix of dimension (xdim, time_dimension) containing the "
             "dynamic regression coefficients.")
+        .def("add_data",
+             [](DynamicRegressionModel &model,
+                const Ptr<RegressionDataTimePoint> &time_point) {
+               model.add_data(time_point); },
+             py::arg("time_point"),
+             "Add the time point as the most recent time point informing the "
+             "model. \n")
         .def("coef", &DynamicRegressionModel::coef, py::arg("t"),
              "Args:\n\n"
              "  t: Time index.  A positive number less than time_dimension.")
@@ -66,6 +79,76 @@ namespace BayesBoom {
             "residual SD to get the actual innovation standard deviations.")
         ;
 
+
+    py::class_<DynamicRegressionDirectGibbsSampler,
+               PosteriorSampler,
+               Ptr<DynamicRegressionDirectGibbsSampler>>(
+                   boom, "DynamicRegressionDirectGibbsSampler")
+        .def(py::init(
+            [] (Ptr<DynamicRegressionModel> model,
+                double residual_sd_prior_guess,
+                double residual_sd_prior_sample_size,
+                const Vector &innovation_sd_prior_guess,
+                const Vector &innovation_sd_prior_sample_size,
+                const Vector &prior_inclusion_probabilities,
+                const Vector &expected_inclusion_duration,
+                const Vector &transition_probability_prior_sample_size,
+                RNG &seeding_rng) {
+              return new DRDGS(
+                  model.get(),
+                  residual_sd_prior_guess,
+                  residual_sd_prior_sample_size,
+                  innovation_sd_prior_guess,
+                  innovation_sd_prior_sample_size,
+                  prior_inclusion_probabilities,
+                  expected_inclusion_duration,
+                  transition_probability_prior_sample_size,
+                  seeding_rng);
+            }),
+             py::arg("model"),
+             py::arg("residual_sd_prior_sample_size"),
+             py::arg("residual_sd_prior_guess"),
+             py::arg("innovation_sd_prior_sample_size"),
+             py::arg("innovation_sd_prior_guess"),
+             py::arg("prior_inclusion_probabilities"),
+             py::arg("expected_inclusion_duratione"),
+             py::arg("transition_probability_prior_sample_size"),
+             py::arg("seeding_rng") = BOOM::GlobalRng::rng,
+             "Args:\n\n"
+             "  model: The model to be sampled.\n"
+             "  residual_sd_prior_guess: An a priori estimate of the residual "
+             "standard deviation parameter.\n"
+             "  residual_sd_prior_sample_size: Number of observations worth of "
+             "weight to put on residual_sd_prior_guess.\n"
+             "  innovation_sd_prior_guess:  A vector containing a priori "
+             "estimates of the 'unscaled' standard deviations describing the "
+             "period-to-period changes in the regression coefficients.  \n"
+             "  innovation_sd_prior_sample_size: The number of observations "
+             "worth of weight to place on residual_sd_prior_guess.\n"
+             "  prior_inclusion_probabilities:  A Vector containing a priori "
+             "estimates of the steady state inclusion probability for each "
+             "coefficient.\n"
+
+             "  expected_inclusion_duration: A vector of positive numbers "
+             "giving the expected duration of an inclusion event.  If the "
+             "prior inclusion probability for a coefficient is less than 0.5 "
+             "then this can be any number larger than 1.  Otherwise, the "
+             "expected duration is constrained.  TODO: describe the constraint."
+             "\n"
+             "  transition_probability_prior_sample_size:  A vector of positive"
+             " numbers giving the number of observations worth of weight to "
+             "place on the prior inclusion probabilities and expected "
+             "durations.\n"
+             "  seeding_rng:  The random number generator used to seed the "
+             "RNG in this sampler.\n")
+        .def("draw", &DRDGS::draw,
+             "Perform one posterior sampling iteration on the managed model.\n")
+        .def("draw_residual_variance", &DRDGS::draw_residual_variance)
+        .def("draw_inclusion_indicators", &DRDGS::draw_inclusion_indicators)
+        .def("draw_transition_probabilities",
+             &DRDGS::draw_transition_probabilities)
+        .def("draw_residual_variance", &DRDGS::draw_residual_variance)
+        ;
   }  // StateSpaceModel_def
 
 }  // namespace BayesBoom
--- Interfaces/python/BayesBoom/Models/StateSpace/dynreg_test.py
+++ Interfaces/python/BayesBoom/Models/StateSpace/dynreg_test.py
@@ -2,9 +2,24 @@ import unittest
 import BayesBoom as boom
 import numpy as np
 
+
 class DynregTest(unittest.TestCase):
     def setUp(self):
         pass
+
+    def simulate_null_data(self, time_dimension, typical_sample_size, xdim):
+        ans = []
+        sample_sizes = np.random.poisson(typical_sample_size, time_dimension)
+        for i in range(time_dimension):
+            time_point = boom.RegressionDataTimePoint(xdim)
+            sample_size = sample_sizes[i]
+            responses = np.random.randn(sample_size)
+            predictors = np.random.randn(sample_size, xdim)
+            for j in range(sample_size):
+                reg_data = boom.RegressionData(responses[i], predictors[i, :])
+                time_point.add_data(reg_data)
+            ans.append(time_point)
+        return ans
 
     def test_data(self):
         time_point = boom.RegressionDataTimePoint()
@@ -22,15 +37,43 @@ class DynregTest(unittest.TestCase):
         self.assertEqual(11, time_point.sample_size)
 
     def test_model(self):
-        model = boom.DynamicRegressionModel(3)
+        xdim = 3
+        typical_sample_size = 30
+        time_dimension = 12
+        model = boom.DynamicRegressionModel(xdim)
 
+        data = self.simulate_null_data(
+            time_dimension, typical_sample_size, xdim)
 
-        # add data
+        for i in range(len(data)):
+            model.add_data(data[i])
 
-        # set posterior sampler
+        sampler = boom.DynamicRegressionDirectGibbsSampler(
+            model,
+            1.0,
+            1.0,
+            boom.Vector(np.array([1.0] * xdim)),
+            boom.Vector(np.array([1.0] * xdim)),
+            boom.Vector(np.array([.25] * xdim)),
+            boom.Vector(np.array([2.0] * xdim)),
+            boom.Vector(np.array([1.0] * xdim)),
+            boom.GlobalRng.rng
+        )
 
-        #
+        model.set_method(sampler)
 
+        for i in range(10):
+            model.sample_posterior()
 
-if __name__ == "__main__":
+debug_mode_ = False
+
+if debug_mode_:
+#     import pdb
+    rig = DynregTest()
+    rig.setUp()
+#     pdb.set_trace()
+    rig.test_data()
+    rig.test_model()
+
+elif __name__ == "__main__":
     unittest.main()
