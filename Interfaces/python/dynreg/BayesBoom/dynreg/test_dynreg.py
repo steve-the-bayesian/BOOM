ff8bc67d21d9efe6d6eebe31603e2d0889dcc806
steve-the-bayesian <steve.the.bayesian@gmail.com>
2021-08-18 14:01:41 -0700

    dynreg testing

--- Interfaces/python/dynreg/BayesBoom/dynreg/test_dynreg.py
+++ Interfaces/python/dynreg/BayesBoom/dynreg/test_dynreg.py
@@ -1,6 +1,12 @@
 import unittest
 import numpy as np
-import BayesBoom.boom as boom
+import pandas as pd
+import BayesBoom.dynreg as dynreg
+import BayesBoom.spikeslab as ss
+import BayesBoom.R as R
+import matplotlib.pyplot as plt
+
+import pdb
 
 
 class TestDynamicRegression(unittest.TestCase):
@@ -38,12 +44,68 @@ class TestDynamicRegression(unittest.TestCase):
                 )
 
         data = []
+        xnames = ["X" + str(i) for i in range(xdim)]
+        xnames[0] = "(Intercept)"
         for t in range(time_dimension):
             sample_size = np.random.poisson(typical_sample_size, 1)[0]
             X = np.random.randn(sample_size, xdim)
             X[:, 0] = 1.0
             yhat = X @ coefficients[:, t]
             y = yhat + residual_sd * np.random.randn(sample_size)
-            data.append(boom.RegressionDataTimePoint(boom.Matrix(X),
-                                                     boom.Vector(y)))
-        return data, coefficients, inclusion
+            chunk = pd.DataFrame(X, columns=xnames)
+            chunk["y"] = y
+            chunk["timestamp"] = t
+            data.append(chunk)
+
+        full_data = pd.concat(data, axis=0, ignore_index=True)
+        return full_data, coefficients, inclusion
+
+    def test_mcmc(self):
+        xdim = 4
+        data, coefficients, inclusion = self.simulate_data_from_model(
+            time_dimension=200,
+            typical_sample_size=50,
+            xdim=xdim,
+            residual_sd=.25,
+            unscaled_innovation_sd=np.array([.01] * xdim),
+            p00=np.array([.95] * xdim),
+            p11=np.array([.99] * xdim),
+        )
+        model = dynreg.SparseDynamicRegressionModel(
+            "y ~ " + ss.dot(data, ["y", "timestamp", "(Intercept)"]),
+            data=data,
+            timestamps="timestamp",
+            niter=100)
+        import pdb
+        pdb.set_trace()
+        print("foo")
+
+
+_debug_mode = True
+
+if _debug_mode:
+    import pdb  # noqa
+
+    # Turn warnings into errors.
+#    import warnings
+#    warnings.simplefilter("error")
+
+    # Run the test you are trying to debug here.  Instantiate the test class,
+    # then call the problematic test.  Call pdb.pm() in the event of an
+    # exception.
+    print("Hello, world!")
+
+    rig = TestDynamicRegression()
+
+    if hasattr(rig, "setUpClass"):
+        rig.setUpClass()
+    if hasattr(rig, "setUp"):
+        rig.setUp()
+
+    rig.test_mcmc()
+
+    print("Goodbye, cruel world!")
+
+else:
+    if __name__ == "__main__":
+        unittest.main(verbosity=2)
