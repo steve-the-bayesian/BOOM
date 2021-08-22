873a74657fca8d1460e0ba52fd91ba3925a007c8
steve-the-bayesian <steve.the.bayesian@gmail.com>
2021-08-21 17:02:21 -0700

    Unit test for dynamic regression.

--- Interfaces/python/dynreg/BayesBoom/dynreg/test_dynreg.py
+++ Interfaces/python/dynreg/BayesBoom/dynreg/test_dynreg.py
@@ -1,12 +1,13 @@
 import unittest
 import numpy as np
 import pandas as pd
+import BayesBoom.boom as boom
 import BayesBoom.dynreg as dynreg
 import BayesBoom.spikeslab as ss
 import BayesBoom.R as R
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-import pdb
+# import pdb
 
 
 class TestDynamicRegression(unittest.TestCase):
@@ -62,11 +63,12 @@ class TestDynamicRegression(unittest.TestCase):
 
     def test_mcmc(self):
         xdim = 4
+        true_residual_sd = .25
         data, coefficients, inclusion = self.simulate_data_from_model(
             time_dimension=200,
-            typical_sample_size=50,
+            typical_sample_size=5000,
             xdim=xdim,
-            residual_sd=.25,
+            residual_sd=true_residual_sd,
             unscaled_innovation_sd=np.array([.01] * xdim),
             p00=np.array([.95] * xdim),
             p11=np.array([.99] * xdim),
@@ -75,13 +77,35 @@ class TestDynamicRegression(unittest.TestCase):
             "y ~ " + ss.dot(data, ["y", "timestamp", "(Intercept)"]),
             data=data,
             timestamps="timestamp",
-            niter=100)
-        import pdb
-        pdb.set_trace()
-        print("foo")
+            niter=100,
+            residual_precision_prior=R.SdPrior(true_residual_sd, 1),
+            seed=8675309)
+
+        for i in range(4):
+            self.assertEqual("", boom.check_stochastic_process(
+                boom.Matrix(model._beta_draws[:, i, :]),
+                boom.Vector(coefficients[i, :]),
+                confidence=.95,
+                sd_ratio_threshold=10000,  # Turn off the sd_ratio check.
+            ))
+
+        posterior_mean_residual_sd = np.mean(model._residual_sd_draws[10:])
+        self.assertGreater(posterior_mean_residual_sd, true_residual_sd - .02)
+        self.assertLess(posterior_mean_residual_sd, true_residual_sd + .02)
+
+        # fig, ax = plt.subplots(2, 2)
+        # which = 0
+        # for i in range(2):
+        #     for j in range(2):
+        #         R.plot_dynamic_distribution(model._beta_draws[:, which, :],
+        #                                     ax=ax[i, j])
+        #         R.lines(np.arange(coefficients.shape[1]),
+        #                 coefficients[which, :], color="blue", ax=ax[i, j])
+        #         which += 1
+        # fig.show()
 
 
-_debug_mode = True
+_debug_mode = False
 
 if _debug_mode:
     import pdb  # noqa
