19c86463b279456e08cb871fd71b5ff8311f3a7a
Steven L. Scott <steve.the.bayesian@gmail.com>
2024-06-25 12:51:10 -0700

    Added a code timer.

--- cpputil/timer.hpp
+++ cpputil/timer.hpp
@@ -19,6 +19,10 @@
 */
 
 #include <chrono>
+#include <ostream>
+#include <iomanip>
+#include "LinAlg/Vector.hpp"
+#include "math/Permutation.hpp"
 
 namespace BOOM {
 
@@ -34,7 +38,7 @@ namespace BOOM {
     void start() {
       start_time_ = clock_.now();
     }
-    
+
     void stop() {
       stop_time_ = clock_.now();
       std::chrono::duration<double> dt = stop_time_ - start_time_;
@@ -44,7 +48,7 @@ namespace BOOM {
     void clear() {
       elapsed_time_ = 0.0;
     }
-    
+
     double time_in_seconds() const {
       return elapsed_time_;
     }
@@ -54,6 +58,73 @@ namespace BOOM {
     std::chrono::time_point<std::chrono::high_resolution_clock> start_time_;
     std::chrono::time_point<std::chrono::high_resolution_clock> stop_time_;
     double elapsed_time_;
+  };
+
+  //===========================================================================
+  class TimeRecorder {
+   public:
+    void record_elapsed_time(const std::string &category, double seconds) {
+      seconds_per_category_[category] += seconds;
+    }
+
+    std::ostream & print(std::ostream &out) const {
+      std::vector<std::string> categories;
+      Vector times;
+      for (const auto &el : seconds_per_category_) {
+        categories.push_back(el.first);
+        times.push_back(el.second);
+      }
+
+      Permutation<Int> order = Permutation<Int>::order(times);
+      for (Int i = 0; i < order.size(); ++i) {
+        out << std::setprecision(4) << std::setw(12) << times[order[i]]
+        << "   " << categories[order[i]] << "\n";
+      }
+      out << std::flush;
+      return out;
+    }
+
+    double operator[](const std::string &label) const {
+      auto it = seconds_per_category_.find(label);
+      if (it == seconds_per_category_.end()) {
+        return negative_infinity();
+      } else {
+        return it->second;
+      }
+    }
+
+   private:
+    std::map<std::string, double> seconds_per_category_;
+  };
+
+  std::ostream & operator<<(std::ostream &out, const TimeRecorder &recorder) {
+    return recorder.print(out);
+  }
+
+  //===========================================================================
+  // A Timer that can be started at the beginning of a function or code block.
+  // When the timer goes out of scope it records in 'recorder' the amount of
+  // time since its creation.
+  class ScopedTimer : Timer {
+   public:
+
+    // Args:
+    //   recorder: The TimeRecorder that will record the timer's value when it
+    //     goes out of scope.
+    //   category:  The label that recorder will use to record the elapsed time.
+    ScopedTimer(TimeRecorder *recorder, const std::string &category)
+        : recorder_(recorder),
+          category_(category)
+    {}
+
+    ~ScopedTimer() {
+      stop();
+      recorder_->record_elapsed_time(category_, time_in_seconds());
+    }
+
+   private:
+    TimeRecorder *recorder_;
+    std::string category_;
   };
 }  // namespace BOOM
 
