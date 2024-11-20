60b693399d84837669e5e1513596f4f38d934b39
steve-the-bayesian <steve.the.bayesian@gmail.com>
2024-11-20 12:49:44 -0800

    Add test file for Date class.

---
+++ cpputil/test/date_test.cc
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "cpputil/Date.hpp"
+
+namespace {
+  using namespace BOOM;
+  using std::string;
+  using std::endl;
+
+  TEST(DateTest, TestMonthNameFromInteger) {
+    int year = 2020;
+    int month = 4;
+    int day = 12;
+    Date d(year, MonthNames(month), day);
+  }
+
+
+
+}  // namespace
